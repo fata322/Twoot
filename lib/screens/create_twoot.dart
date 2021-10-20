6d32069c4dea6c152d39762be29ceba54acdba46
Marc Pacaldo <fatafatafata322@gmail.com>
2021-10-20 20:39:35 +0800

    minimal code format changes

--- lib/screens/create_twoot.dart
+++ lib/screens/create_twoot.dart
@@ -28,6 +28,7 @@ class _CreateTwootState extends State<CreateTwoot> {
         child: TextField(
           controller: twootText,
           maxLength: 240,
+          
           onChanged: (value) {
             setState(() {
               _enteredText = value;
@@ -46,6 +47,7 @@ class _CreateTwootState extends State<CreateTwoot> {
               fillColor: Color(0xff000000),
               focusColor: Color(0xffffffff),
               hintText: "What's happening?",
+              
               hintStyle: TextStyle(
                 fontWeight: FontWeight.w200,
                 color: Color(0xffffffff).withOpacity(0.5),
@@ -53,6 +55,7 @@ class _CreateTwootState extends State<CreateTwoot> {
         ),
       )),
       backgroundColor: Color(0x000000),
+     
       appBar: AppBar(
           automaticallyImplyLeading: false,
           elevation: 0,
@@ -63,9 +66,7 @@ class _CreateTwootState extends State<CreateTwoot> {
               RichText(
                   text: TextSpan(
                       text: 'Cancel',
-                      style: TextStyle(
-                          fontWeight: FontWeight.w600,
-                          color: Color(0xff08a0e9)),
+                      style: TextStyle(fontWeight: FontWeight.w600,color: Color(0xff08a0e9)),
                       recognizer: TapGestureRecognizer()
                         ..onTap = () => Navigator.pop(
                             context,
@@ -80,8 +81,8 @@ class _CreateTwootState extends State<CreateTwoot> {
                       MaterialPageRoute(builder: (context) => Dashboard()));
                   setState(() {
                     twootText.clear();
-                    ScaffoldMessenger.of(context).showSnackBar(
-                        SnackBar(content: Text('Twooted Successfully')));
+                    ScaffoldMessenger.of(context)
+                        .showSnackBar(SnackBar(content: Text('Twooted Successfully')));
                   });
                 },
                 child: Text('Twoot!',
